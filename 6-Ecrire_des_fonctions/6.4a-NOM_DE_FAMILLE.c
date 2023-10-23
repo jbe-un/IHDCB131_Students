203e69641b3673e8184d2337515526c67959d127
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-23 11:24:58 +0200

    Add exercises for chapter 6

---
+++ 6-Ecrire_des_fonctions/6.4a-NOM_DE_FAMILLE.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// TODO: définir une fonction qui calcule la somme des diviseurs d'un nombre et qui sera appelée dans la fonction "areFriends"
+
+// Préconditions: num1 and num2 are natural
+// Postconditions: returns true if nb1 and nb2 are amicals, false otherwise
+int areFriends(int nb1, int nb2) {
+    // TODO: calculer si les nombres sont amicaux ou pas
+}
+
+// Ne pas modifier la ligne suivante
+#ifndef TEST_IHDCB131
+
+int main() {
+    int nb1, nb2;
+    // TODO: récupérer les deux nombres encodés par l'utilisateur et calculer s'ils sont amicaux ou pas
+
+    return 0;
+}
+
+// Ne pas modifier la ligne suivante
+#endif
---
+++ 6-Ecrire_des_fonctions/6.4b-NOM_DE_FAMILLE.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// TODO: définir la fonction "displayRowOfStars" qui prend en argument un entier et qui affiche sur une ligne le nombre d'étoiles correspondant à cet entier
+
+// TODO: définir la fonction "displaySquareOfStars" qui prend en argument un entier et qui affiche un carré de taille n avec uniquement des étoiles
+
+// Ne pas modifier la ligne suivante
+#ifndef TEST_IHDCB131
+
+int main() {
+    int borneMin, borneMax;
+    // TODO: récupérer les deux bornes et afficher tous les carrés d'étoiles dont la taille se situe entre les deux bornes entrées
+
+    return 0;
+}
+
+// Ne pas modifier la ligne suivante
+#endif
