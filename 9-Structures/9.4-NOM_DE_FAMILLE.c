e1c5c53ac7fa06bc64de9f41ba558843899a45fe
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-24 15:11:06 +0200

    Add exercise for chapter 9

---
+++ 9-Structures/9.4-NOM_DE_FAMILLE.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// TODO: structure pour le morceau et enum pour le type de morceau
+struct MusicPiece {
+
+};
+
+// Préconditions: TODO
+// Postconditions: TODO
+void encode_music_piece(struct MusicPiece *music_piece) {
+
+}
+
+// Préconditions: TODO
+// Postconditions: TODO
+void display_average_length(struct MusicPiece *music_pieces, int number_music_pieces) {
+
+}
+
+// Préconditions: TODO
+// Postconditions: TODO
+void display_longest_music_piece(struct MusicPiece *music_pieces, int number_music_pieces) {
+
+}
+
+// Préconditions: TODO
+// Postconditions: TODO
+void display_longest_suite(struct MusicPiece *music_pieces, int number_music_pieces) {
+
+}
+
+
+// Ne pas modifier la ligne suivante
+#ifndef TEST_IHDCB131
+
+int main (int argc,char** argv)
+{
+    int number_pieces;
+    printf("Nombre de morceaux à encode: ");
+    scanf("%d",&number_pieces);
+
+    // TODO: à compléter
+
+    return 0;
+}
+
+// Ne pas modifier la ligne suivante
+#endif
