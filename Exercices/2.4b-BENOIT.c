efc7b35fa3ffb0d908f3657cfa7aac70c2f160c7
jbe-un <j.be@outlook.be>
2024-10-09 17:47:00 +0200

    Update 2.4b-BENOIT.c
    ex

---
+++ Exercices/2.4b-BENOIT.c
@@ -0,0 +1,22 @@
+    // TODO: afficher une phrase demandant d'encoder les valeurs des trois nombres et récupérer leur valeur
+    printf ("Encodez trois valeurs : \n");
+    printf ("La valeur a:\n");
+    scanf ("%i",&a);
+    printf ("Encodez la valeur b:\n");
+    scanf ("%i",&b);
+    printf ("Encodez la valeur c:\n");
+    scanf ("%i",&c);
+    // TODO: calculer la médiane des trois nombres
+    if ((a > b && a < c) || (a < b && a > c)){
+        mediane = a;
+    }
+    if ((b > a && b < c) || (b < a && b > c)){
+        mediane = b;
+    }
+    else {
+        mediane = c;
+    }        
+    // TODO: afficher une phrase montrant la médiane des trois nombres
+    printf ("la valeur médiane de vos nombres %i,%i et %i est %i\n", a,b,c,mediane);
+    return 0;
+}
